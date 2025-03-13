3a1adf1179b02d637bb47d9ff13c06dae7ec9c9c
MoeTheLawn28 <165844183+MoeTheLawn28@users.noreply.github.com>
2025-03-13 14:13:05 +0200

    Add project files.

---
+++ IceTask1/IShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask1
+{
+    public interface IShape
+    {
+        double CalculateArea();  // Method to calculate area
+    }
+}
---
+++ IceTask1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IceTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
